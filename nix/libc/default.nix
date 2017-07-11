212fb1787304840107ea19f79a5c9b77d6e7c304
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-07-11 20:26:07 +0300

    Update derivation syntax

--- nix/libc/default.nix
+++ nix/libc/default.nix
@@ -1,15 +1,15 @@
-{ pkgs }:
+{ pkgs, target, prefix }:
 pkgs.stdenv.mkDerivation {
   name = "libc";
   buildDepends = [pkgs.flex pkgs.gmp pkgs.mpfr pkgs.libmpc];
   src = pkgs.fetchurl {
     url = "http://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz";
-    md5 = "70c9ccf9fac07f762c24f2df2290784d"; 
+    sha256 = "1n2vzs8qv6bzx85sln74raxgh7nnwim00pkznhd3276fshll0cnb";
   };
   unpackPhase = ''
     tar xzf binutils-2.28.tar.gz && rm binutils-2.28.tar.gz;
   '';
   buildPhase = ''
-    $src/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+    $src/configure --target=$target --prefix="$prefix" --with-sysroot --disable-nls --disable-werror
   '';
 }
